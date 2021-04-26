0e20119fb0ea57602804887d5c01da4affccaadb
pmzul <pmzulian@gmail.com>
2021-04-26 19:46:38 -0300

    Componentes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
 
-  const titulo = "Bienvenido al Blog en React";
-  const persona = {
-    nombre: "Mario BROS",
-    edad: 37
-  }
-  const fce = "http://fce.uncuyo.edu.ar/";
   return (
     <div className="App">
       
-      <div className="content">
-        <h1>{titulo}</h1>
-        <h2>Blog de {persona.nombre}, de {persona.edad} años de edad</h2>
-        <p>Aleatorio: {Math.random()*10}</p>
+      <Navbar />
 
-        <a href={fce} target="_blanck">Facultad de Ciencias Económicas</a>
+      <div className="content">
+
+        <Home />
+
       </div>
 
     </div>
