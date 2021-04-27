f1e7718bf61035b46ad808fe2a817ae1da30fa37
pmzul <pmzulian@gmail.com>
2021-04-26 22:03:28 -0300

    Eventos

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log("Evento clic capturado", e);
+    }
+
+    const handleClickAgain = (param, e) => {
+        console.log("Alerta con parámetro " + param, e.target)
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <br/>
+            <button onClick={handleClick}>Hacer clic</button>
+            <br/>
+            <button onClick={(e) => {handleClickAgain('¡CUIDADO!', e)}}>Otro clic</button>
         </div>
     );
 }
